8bc4c670e21ad934ddeb90d2ee717da854686b38
codehans <94654388+codehans@users.noreply.github.com>
2022-07-07 18:10:26 +0100

    Impl Decimal256 round

--- src/precision.rs
+++ src/precision.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Decimal, Fraction, Uint128};
+use cosmwasm_std::{Decimal, Decimal256, Fraction, Uint128, Uint256};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -41,6 +41,26 @@ impl Precise for Decimal {
                 let pow = Uint128::from(10u128).pow(18 - *dp as u32);
                 let x = Self::from_ratio(self.numerator(), Uint128::from(self.denominator()) * pow);
                 Self::from_ratio(Uint128::from(x.numerator()) * pow, x.denominator())
+            }
+        }
+    }
+}
+
+impl Precise for Decimal256 {
+    fn round(&self, p: &Precision) -> Self {
+        match p {
+            Precision::SignificantFigures(sf) => {
+                let int = self.numerator();
+                let len = int.to_string().as_str().bytes().len() as u32;
+                let decimals: u32 = len - *sf as u32;
+                let pow = Uint256::from(10u128).pow(decimals);
+                let truncated = Self::from_ratio(int, pow) * Uint256::from(1u128);
+                Self::from_ratio(truncated * pow, self.denominator())
+            }
+            Precision::DecimalPlaces(dp) => {
+                let pow = Uint256::from(10u128).pow(18 - *dp as u32);
+                let x = Self::from_ratio(self.numerator(), Uint256::from(self.denominator()) * pow);
+                Self::from_ratio(Uint256::from(x.numerator()) * pow, x.denominator())
             }
         }
     }
