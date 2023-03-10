603e60a087639cc605438de6ad1a742f30456d8a
Amit Prasad <17amitprasad@gmail.com>
2023-03-09 18:45:36 -0600

    Clippy lints

--- packages/kujira-std/src/callback.rs
+++ packages/kujira-std/src/callback.rs
@@ -5,11 +5,11 @@ use cosmwasm_std::{Binary, Coin, CosmosMsg, Env, WasmMsg};
 pub struct Callback(pub Binary);
 
 impl Callback {
-    pub fn to_message<T>(&self, env: &Env, funds: &Vec<Coin>) -> CosmosMsg<T> {
+    pub fn to_message<T>(&self, env: &Env, funds: &[Coin]) -> CosmosMsg<T> {
         CosmosMsg::Wasm(WasmMsg::Execute {
             contract_addr: env.contract.address.to_string(),
             msg: self.0.clone(),
-            funds: funds.clone(),
+            funds: funds.to_owned(),
         })
     }
 }
