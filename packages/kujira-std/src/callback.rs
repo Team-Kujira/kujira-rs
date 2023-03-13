52fac0485fb4cda0959df5565816528f7a48e100
Amit Prasad <17amitprasad@gmail.com>
2023-03-13 08:30:03 -1000

    Callback update

--- packages/kujira-std/src/callback.rs
+++ packages/kujira-std/src/callback.rs
@@ -1,15 +1,19 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{Binary, Coin, CosmosMsg, Env, WasmMsg};
+use cosmwasm_std::{Addr, Binary, Coin, CosmosMsg, WasmMsg};
 
 #[cw_serde]
 pub struct Callback(pub Binary);
 
 impl Callback {
-    pub fn to_message<T>(&self, env: &Env, funds: &[Coin]) -> CosmosMsg<T> {
+    pub fn to_message<T>(&self, cb_addr: &Addr, funds: &[Coin]) -> CosmosMsg<T> {
         CosmosMsg::Wasm(WasmMsg::Execute {
-            contract_addr: env.contract.address.to_string(),
+            contract_addr: cb_addr.to_string(),
             msg: self.0.clone(),
             funds: funds.to_owned(),
         })
+    }
+
+    pub fn into_binary(self) -> Binary {
+        self.0
     }
 }
