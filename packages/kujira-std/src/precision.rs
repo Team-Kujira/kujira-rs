38089ebf7315619948170d9c97ff737688e398e8
Christoph Otter <chris@confio.gmbh>
2024-04-22 15:31:51 +0200

    Add tests for Precision::round

--- packages/kujira-std/src/precision.rs
+++ packages/kujira-std/src/precision.rs
@@ -92,4 +92,61 @@ mod tests {
         assert_eq!(p.validate(&Decimal::from_str("12.343").unwrap()), None);
         assert_eq!(p.validate(&Decimal::from_str("1.2").unwrap()), Some(()));
     }
+
+    #[test]
+    fn test_significant_figures_round() {
+        const CASES: [(u8, &str, &str); 7] = [
+            (2, "123.456", "120"),
+            (4, "12.345678910111213141", "12.34"),
+            (18, "0.123456789101213141", "0.123456789101213141"),
+            (2, "0.123456", "0.12"),
+            (18 + 3, "123.456", "123.456"),
+            (0, "123.456", "0"),
+            (1, "123", "100"),
+        ];
+
+        for (precision, unrounded, expected) in CASES {
+            let p = Precision::SignificantFigures(precision);
+            assert_eq!(
+                Decimal::from_str(unrounded).unwrap().round(&p),
+                Decimal::from_str(expected).unwrap()
+            );
+            assert_eq!(
+                Decimal256::from_str(unrounded).unwrap().round(&p),
+                Decimal256::from_str(expected).unwrap()
+            );
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_significant_figures_round_panic() {
+        // not sure if this is intended behavior, but it's what the old code did
+        let p = Precision::SignificantFigures(18 + 4);
+        _ = Decimal::from_str("123.456").unwrap().round(&p);
+    }
+
+    #[test]
+    fn test_decimal_places_round() {
+        const CASES: [(u8, &str, &str); 6] = [
+            (2, "123.456", "123.45"),
+            (4, "12.345678910111213141", "12.3456"),
+            (18, "0.123456789101213141", "0.123456789101213141"),
+            (2, "0.123456", "0.12"),
+            (0, "123.456", "123"),
+            (1, "123", "123"),
+        ];
+
+        for (precision, unrounded, expected) in CASES {
+            let p = Precision::DecimalPlaces(precision);
+            assert_eq!(
+                Decimal::from_str(unrounded).unwrap().round(&p),
+                Decimal::from_str(expected).unwrap()
+            );
+            assert_eq!(
+                Decimal256::from_str(unrounded).unwrap().round(&p),
+                Decimal256::from_str(expected).unwrap()
+            );
+        }
+    }
 }
