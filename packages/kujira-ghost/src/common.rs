033d0161b656b59b58c3e06d3160955cc9057204
Amit Prasad <mail@amitprasad.dev>
2024-05-28 17:07:43 +0200

    OracleType util

--- packages/kujira-ghost/src/common.rs
+++ packages/kujira-ghost/src/common.rs
@@ -1,8 +1,18 @@
 use cosmwasm_schema::cw_serde;
-use kujira_std::HumanPrice;
+use cosmwasm_std::{QuerierWrapper, StdResult};
+use kujira_std::{HumanPrice, KujiraQuerier, KujiraQuery};
 
 #[cw_serde]
 pub enum OracleType {
     Static(HumanPrice),
     Live(String),
+}
+
+impl OracleType {
+    pub fn price(&self, querier: &QuerierWrapper<KujiraQuery>) -> StdResult<HumanPrice> {
+        match self {
+            OracleType::Static(price) => Ok(*price),
+            OracleType::Live(ref key) => KujiraQuerier::new(querier).query_exchange_rate(key),
+        }
+    }
 }
